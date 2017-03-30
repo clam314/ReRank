5030b50de5cac5261c834d05c5213d078b019a68
wuweimin <clam314@163.com>
2017-03-30 18:26:27 +0800

    Initial commit

---
+++ app/src/main/java/com/clam314/rerank/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.clam314.rerank.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.clam314.rerank.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
---
+++ app/src/main/java/com/clam314/rerank/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.clam314.rerank.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.clam314.rerank.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
